4b52acb0c6f2aea995f1fcbea5a78290d37d1972
GitMaRobSto <m.robitsch@sto.com>
2019-09-10 10:22:45 +0000

    Teste 09_10_03

--- WebApplication1Core/WebApplication1Core/Pages/Marjan.cshtml.cs
+++ WebApplication1Core/WebApplication1Core/Pages/Marjan.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,61 +19,61 @@ namespace WebApplication1Core.Pages
             Random random = new Random();
             int randomNumber = random.Next(0, 100);
 
-            Message = "Nachricht von Marjan! - (" + randomNumber.ToString() + ")"; 
+            Message = "Nachricht von Marjan - Version 1.0.0  - (" + randomNumber.ToString() + ")"; 
         }
 
 
         // test
 
         //// der geht mit Parameter
-        //private void createPostgreConn2()
-        //{
+        private void createPostgreConn2()
+        {
 
-        //    //CREATE OR REPLACE FUNCTION getAdressTest_Net1()
-        //    //  RETURNS TABLE(id uuid, name varchar, surname varchar) AS $$
-        //    //BEGIN
-        //    // RETURN QUERY
-        //    // SELECT app_adress.id, app_adress.name, app_adress.surname FROM public.app_adress;
-        //    //END; $$ 
+            //CREATE OR REPLACE FUNCTION getAdressTest_Net1()
+            //  RETURNS TABLE(id uuid, name varchar, surname varchar) AS $$
+            //BEGIN
+            // RETURN QUERY
+            // SELECT app_adress.id, app_adress.name, app_adress.surname FROM public.app_adress;
+            //END; $$ 
 
-        //    //LANGUAGE 'plpgsql';
+            //LANGUAGE 'plpgsql';
 
 
-        //    //Select getAdressTest_Net1();
+            //Select getAdressTest_Net1();
 
-        //    // PostgreSQL-style connection string
-        //    string connstring = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
-        //            "deweas96.de.intra.sto.com", "5432", "KaffeeKassenAppUser", "123456", "kaffeekassenapp");
-        //    // Making connection with Npgsql provider
-        //    NpgsqlConnection conn = new NpgsqlConnection(connstring);
+            // PostgreSQL-style connection string
+            string connstring = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                    "deweas96.de.intra.sto.com", "5432", "KaffeeKassenAppUser", "123456", "kaffeekassenapp");
+            // Making connection with Npgsql provider
+            NpgsqlConnection conn = new NpgsqlConnection(connstring);
 
-        //    conn.Open();
-        //    NpgsqlTransaction tran = conn.BeginTransaction();
+            conn.Open();
+            NpgsqlTransaction tran = conn.BeginTransaction();
 
-        //    //NpgsqlCommand command = new NpgsqlCommand("getAdressTest_Net1", conn);
-        //    NpgsqlCommand command = new NpgsqlCommand("getAdressTest_Net", conn);
-        //    command.CommandType = CommandType.StoredProcedure;
-        //    //command.Parameters.AddWithValue(":id", new Guid());
-        //    //command.Parameters.AddWithValue(":name", "Hallo1");
-        //    //command.Parameters.AddWithValue(":surname", "Hallo2");
-        //    //command.Parameters.AddWithValue(":i", 5);
-        //    command.ExecuteNonQuery();
-        //    NpgsqlDataAdapter da = new NpgsqlDataAdapter(command);
+            //NpgsqlCommand command = new NpgsqlCommand("getAdressTest_Net1", conn);
+            NpgsqlCommand command = new NpgsqlCommand("getAdressTest_Net", conn);
+            command.CommandType = CommandType.StoredProcedure;
+            //command.Parameters.AddWithValue(":id", new Guid());
+            //command.Parameters.AddWithValue(":name", "Hallo1");
+            //command.Parameters.AddWithValue(":surname", "Hallo2");
+            //command.Parameters.AddWithValue(":i", 5);
+            command.ExecuteNonQuery();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(command);
 
-        //    PostgresDataSet psDataset = new PostgresDataSet();
-        //    da.Fill(psDataset, "app_adress");
+            //PostgresDataSet psDataset = new PostgresDataSet();
+            //da.Fill(psDataset, "app_adress");
 
-        //    NpgsqlDataReader dr = command.ExecuteReader();
-        //    //while (dr.Read())
-        //    //{
-        //    //    // do what you want with data, convert this to json or...
-        //    //    Console.WriteLine(dr[0]);
-        //    //}
-        //    dr.Close();
+            NpgsqlDataReader dr = command.ExecuteReader();
+            //while (dr.Read())
+            //{
+            //    // do what you want with data, convert this to json or...
+            //    Console.WriteLine(dr[0]);
+            //}
+            dr.Close();
 
-        //    tran.Commit();
-        //    conn.Close();
-        //}
+            tran.Commit();
+            conn.Close();
+        }
 
     }
 }
